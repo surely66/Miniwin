ae0a375b5d84b109acc4588489cfae3719315ff9
houzh <houzh.qq.com>
2020-02-11 10:25:32 +0800

    add rfb to env{USE_RFB_GRAPH} control

--- mtienv.sh
+++ mtienv.sh
@@ -1,4 +1,4 @@
 
-export LD_LIBRARY_PATH=/mnt/nfs/alitest/out-mti-ali3528/lib:/usr/lib
-export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/mnt/nfs/alitest/out-mti-ali3528/bin
+export LD_LIBRARY_PATH=/mnt/nfs/alitest/out-ali3528/lib:/usr/lib
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/mnt/nfs/alitest/out-ali3528/bin
 export TSINJECT_FILE=/mnt/nfs/alitest/36E_11823_20181206.ts
