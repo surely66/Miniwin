03fbfab9dd7a3761c0e4be45a2dbcddd23d3261b
houzh <houzh.qq.com>
2020-02-13 16:16:49 +0800

    remove old 3528 build script

--- build3528.sh
+++ build3528.sh
@@ -1,7 +1,8 @@
 mkdir -p out-ali3528
 pushd out-ali3528
-cmake -DCMAKE_TOOLCHAIN_FILE=../ali3528_toolchain.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=../ali3528_mtitoolchain.cmake \
    -DCMAKE_INSTALL_PREFIX=./ \
-   -DDIRECTFB=ON \
+   -DNGL_CHIPSET="ali3528" \
+   -DNGL_CA=acs \
    ..
 popd
--- mtibuild.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p out-ali3528
-pushd out-ali3528
-cmake -DCMAKE_TOOLCHAIN_FILE=../ali3528_mtitoolchain.cmake \
-   -DCMAKE_INSTALL_PREFIX=./ \
-   -DNGL_CHIPSET="ali3528" \
-   -DNGL_CA=acs \
-   ..
-popd
